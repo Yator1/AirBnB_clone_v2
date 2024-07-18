5117137b2660bc656ebe81fbb25c361a01567e08
Yator1 <titusyatz17@gmail.com>
2024-07-18 15:20:42 +0300

    Pycode style correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an 
+Fabric script that distributes an
 archive file to web servers
 """
 
@@ -10,14 +10,15 @@ env.hosts = ['18.210.14.120', '54.90.8.146']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
+
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
         # Extracting the file name and name
-        archive_file = archive_path.split("/")[-1]
-        archive_name = archive_file.split(".")[0]
+        arch_file = archive_path.split("/")[-1]
+        archive_name = arch_file.split(".")[0]
         path = "/data/web_static/releases/"
 
         # Upload the archive to /tmp/ directory
@@ -27,10 +28,10 @@ def do_deploy(archive_path):
         run('mkdir -p {}{}/'.format(path, archive_name))
 
         # Uncompressing the archive to target directory
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_file, path, archive_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(arch_file, path, archive_name))
 
         # Deleting archive from the web server
-        run('rm /tmp/{}'.format(archive_file))
+        run('rm /tmp/{}'.format(arch_file))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name))
         run('rm -rf {}{}/web_static'.format(path, archive_name))
         run('rm -rf /data/web_static/current')
